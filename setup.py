406d01745846fd562e2c472fd04b64e98a2f7383
evgkanias <evgkanias@gmail.com>
2022-11-10 11:47:15 +0000

    fix bug in setup file.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 import os
 
-with open('README.md', 'r', encoding='utd-8') as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
