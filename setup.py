1c40d9494b85967211966173431622749dc1b632
evgkanias <evgkanias@gmail.com>
2022-12-03 14:00:38 +0000

    Update version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fr:
 
 setuptools.setup(
     name="skylight",
-    version="v1.0b1",
+    version="v1.0b2",
     author="Evripidis Gkanias",
     maintainer="Evripidis Gkanias",
     author_email="ev.gkanias@ed.ac.uk",
